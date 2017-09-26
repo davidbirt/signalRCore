dca9b8291fa1650f98d4d5a597d120cfcff5aa71
dbz <dbirt3168@outlook.com>
2017-09-26 13:51:43 -0400

    add pipeline middleware to log off referer header with each request that comes in

--- SignalRCore/Startup.cs
+++ SignalRCore/Startup.cs
@@ -12,6 +12,7 @@ namespace SignalRCore
 {
     public class Startup
     {
+        public IConfigurationRoot Configuration { get; }
         public Startup(IHostingEnvironment env)
         {
             var builder = new ConfigurationBuilder()
@@ -22,7 +23,6 @@ namespace SignalRCore
             Configuration = builder.Build();
         }
 
-        public IConfigurationRoot Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
