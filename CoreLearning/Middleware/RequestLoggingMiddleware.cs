41e7dc328f924f40dc2c58077c148de475f3592f
dbz <dbirt3168@outlook.com>
2017-09-28 13:41:35 -0400

    enhance: adding in a new middleware component for request logging.

---
+++ CoreLearning/Middleware/RequestLoggingMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreLearning.Middleware
+{
+
+    public static class RequestLoggingMiddleWareExtensions
+    {
+        public static IApplicationBuilder UseRequestLogging(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<RequestLoggingMiddleware>();
+        }
+    }
+    public class RequestLoggingMiddleware
+    {
+        readonly RequestDelegate _next;
+        public RequestLoggingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext context)
+        {
+            var referer = context.Request.Headers["referer"];
+            Console.WriteLine(referer);
+
+            return _next(context);
+        }
+    }
+}
--- CoreLearning/Startup.cs
+++ CoreLearning/Startup.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using CoreLearning.Middleware;
 
 namespace CoreLearning
 {
@@ -34,6 +29,9 @@ namespace CoreLearning
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseExceptionHandler();
+            app.UseRequestLogging();
+            app.UseStaticFiles();
             app.UseMvc();
         }
     }
