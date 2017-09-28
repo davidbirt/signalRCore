dc7be356e0524874a8ce2210d82440b8b240d231
dbz <dbirt3168@outlook.com>
2017-09-28 17:36:30 -0400

    enhance: setup project according to the basics so now I can play around with it.

--- CoreLearning/Startup.cs
+++ CoreLearning/Startup.cs
@@ -3,6 +3,12 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using CoreLearning.Middleware;
+using System;
+using System.Net.WebSockets;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using System.Threading;
 
 namespace CoreLearning
 {
@@ -22,8 +28,11 @@ namespace CoreLearning
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logger)
         {
+            logger.AddConsole(LogLevel.Debug);
+            logger.AddDebug(LogLevel.Debug);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -31,8 +40,45 @@ namespace CoreLearning
 
             app.UseExceptionHandler();
             app.UseStaticFiles();
-            app.UseRequestLogging();
-            app.UseMvc();
+
+            app.UseWebSockets(new WebSocketOptions() {
+                KeepAliveInterval = TimeSpan.FromSeconds(180),
+                ReceiveBufferSize = 5 * 1024
+            });
+
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path == "/ws")
+                {
+                    if (context.WebSockets.IsWebSocketRequest)
+                    {
+                        WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                        await Echo(context, webSocket);
+                    }
+                    else
+                    {
+                        context.Response.StatusCode = 400;
+                    }
+                }
+                else
+                {
+                    await next();
+                }
+
+            });
+        }
+
+        private async Task Echo(HttpContext context, WebSocket webSocket)
+        {
+            var buffer = new byte[1024 * 4];
+            WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            while (!result.CloseStatus.HasValue)
+            {
+                await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
+
+                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            }
+            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
         }
     }
 }
